d29f20f2a256602c8753906e928f9027b6d47a0d
吉田大輝 <bob181219@yahoo.co.jp>
2018-03-13 21:04:33 +0900

    Describe function.h

---
+++ function.h
@@ -0,0 +1,18 @@
+#ifndef FUNCTION_H
+#define FUNCTION_H
+
+// standard library
+#include <string>
+#include <iostream>
+
+namespace dfs_cls
+{
+	[[noreturn]] void CommandLine();
+	void script(const std::wstring& filename);
+	inline void welcome()
+	{
+		std::wcout << L"welcome to DFS-CommandLineShell" << endl;
+	}
+}
+
+#endif
