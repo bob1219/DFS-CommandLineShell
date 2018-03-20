49407732591fe9fc729ef99ae9772cb61de0b63b
吉田大輝 <bob181219@yahoo.co.jp>
2018-03-20 20:22:48 +0900

    Describe dfs_cls::script function

---
+++ script.cpp
@@ -0,0 +1,24 @@
+// standard library
+#include <string>
+#include <iostream>
+#include <fstream>
+
+// header
+#include "function.h"
+#include "exception.h"
+
+// using
+using namespace std;
+
+void dfs_cls::script(const wstring& filename)
+{
+	ifstream file;
+	file.imbue(locale{""});
+	file.open(filename);
+	if(file.fail())
+		throw dfs_cls::exception{L"failed open file"};
+
+	wstring line;
+	while(getline(file, line))
+		CommandProcess(line);
+}
