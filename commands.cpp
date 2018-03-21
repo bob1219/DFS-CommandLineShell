eae639cd2fa88df37af0bd90dca57b94f8c5f672
吉田大輝 <bob181219@yahoo.co.jp>
2018-03-21 16:29:58 +0900

    Describe dfs_cls::command::rm function

--- commands.cpp
+++ commands.cpp
@@ -3,6 +3,9 @@
 #include <iostream>
 #include <fstream>
 
+// boost
+#include <boost/filesystem.hpp>
+
 // header
 #include "function.h"
 #include "exception.h"
@@ -15,4 +18,16 @@ void dfs_cls::command::mk(const wstring& filename)
 	wofstream file{filename};
 	if(file.fail())
 		throw dfs_cls::exception{L"failed open file"};
+}
+
+void dfs_cls::command::rm(const wstring& filename)
+{
+	try
+	{
+		remove(filename);
+	}
+	catch(filesystem_error)
+	{
+		throw dfs_cls::exception{L"failed remove"};
+	}
 }
