f6094746ab63d2c334e810a76b530ae2389674ce
吉田大輝 <bob181219@yahoo.co.jp>
2018-03-21 15:36:00 +0900

    Describe declarations of command functions

--- function.h
+++ function.h
@@ -4,6 +4,8 @@
 // standard library
 #include <string>
 #include <iostream>
+#include <regex>
+#include <vector>
 
 namespace dfs_cls
 {
@@ -11,6 +13,30 @@ namespace dfs_cls
 	void script(const std::wstring& filename);
 	inline void welcome() { std::wcout << L"welcome to DFS-CommandLineShell" << endl; }
 	void CommandProcess(const std::wstring& command);
+
+	namespace command
+	{
+		void mk(const std::wstring& filename);
+		void rm(const std::wstring& filename);
+		void cp(const std::wstring& FromFilename, const std::wstring& ToFilename);
+		void basename(const std::wstring& filename);
+		inline void findf(const std::wregex& r) { findf(L".", r); }
+		void findf(const std::wstring& dirname, const std::wregex& r);
+		inline void list() { list(L"."); }
+		void list(const std::wstring& dirname);
+		void mkdir(const std::wstring& dirname);
+		void rmdir(const std::wstring& dirname);
+		void cpdir(const std::wstring& FromDirname, const std::wstring& ToDirname);
+		void rename(const std::wstring& FromName, const std::wstring& ToName);
+		void bview(const std::wstring& filename);
+		void tview(const std::wstring& filename);
+		void cwdir();
+		void info(const std::wstring& filename);
+		void findt(const std::wstring& filename, const wregex& r);
+		void date();
+		void time();
+		void app(const std::vector<std::wstring>& args);
+	}
 }
 
 #endif
